0932442245c29c679a32a530eedd2cd4007beb0d
thegreatraj01 <thegreatraj0810@gmail.com>
2024-07-07 11:15:58 +0530

    utils files video no 8

--- src/db/index.js
+++ src/db/index.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         const connectionInstance = await mongoose.connect(`${process.env.DATABASE_URL}/${DBNAME}`);
         console.log(`\n mongodb Connected  !! DB host  ${connectionInstance.connection.host}`)
     } catch (error) {
-        console.log(`MongoDb connectio error: ${error}`);
+        console.log(`MongoDb connection error: ${error}`);
         throw error;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,45 @@
 import dotenv from "dotenv";
-import express from "express";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config();
 
 
-
-connectDB();
-
-
-
-
-
-
-
-
-
-
-
- /* ;(async () => {
-    try {
-        await mongoose.connect(`${process.env.DATABASE_URL}/${DBNAME}`);
+connectDB()
+    .then(() => {
         app.on('error', (err) => {
             console.log(err);
             throw err;
         });
 
-        app.listen(process.env.PORT, () => {
-            console.log(`app is listening on port  ${process.env.PORT}`)
-        });
-    } catch (error) {
-        console.log(error);
-        throw err;
-    }
+        app.listen(process.env.PORT || 8000, () => console.log(` server is listening on ${process.env.PORT}`));
+    })
+    .catch((err) => console.log(`database connection failed from index.js ${err} `))
+
+
+
+
+
+
+
+
+
+
+
+/* ;(async () => {
+   try {
+       await mongoose.connect(`${process.env.DATABASE_URL}/${DBNAME}`);
+       app.on('error', (err) => {
+           console.log(err);
+           throw err;
+       });
+
+       app.listen(process.env.PORT, () => {
+           console.log(`app is listening on port  ${process.env.PORT}`)
+       });
+   } catch (error) {
+       console.log(error);
+       throw err;
+   }
 })()
 */
