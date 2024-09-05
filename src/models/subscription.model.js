8d4bda97d98ad65c778140f78a2becdf7913f46c
thegreatraj01 <thegreatraj0810@gmail.com>
2024-09-05 22:07:04 +0530

    added subscription model

---
+++ src/models/subscription.model.js
@@ -0,0 +1,15 @@
+import { Schema, model } from "mongoose";
+
+const subscriptonSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId, // one who is subscribing 
+        ref: "User"
+    },
+    channel: {
+        type: Schema.Types.ObjectId, // one to whom subscriber is subscribing
+        ref: "User"
+    }
+}, { timestamps: true })
+
+
+export const Subscription = model("Subscription", subscriptonSchema);
